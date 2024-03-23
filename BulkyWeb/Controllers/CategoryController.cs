ee314f59f69b1fa48fe26f2087b9ea0b8af7f547
Pawan Bhatta <pawanbhatta00@gmail.com>
2024-03-23 14:19:29 +0545

    seeded category data and created read category action method

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _context.Categories.ToList();
+            return View(categories);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
