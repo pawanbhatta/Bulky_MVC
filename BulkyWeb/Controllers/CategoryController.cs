40ead66a7ed86973a53759a8b67d79277d195fec
Pawan Bhatta <pawanbhatta00@gmail.com>
2024-03-23 17:47:49 +0545

    Implemented category update and delete

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -42,5 +42,59 @@ namespace BulkyWeb.Controllers
             }
             return View();
         }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound("Category Not Found");
+            }
+            //different ways of getting category by id
+            //first way
+            Category? category = _context.Categories.Find(id); //only works with primary key
+            //Category? category1 = _context.Categories.FirstOrDefault(u => u.Id == id); //works with any property (here used id, we can also use name or anything)
+            //Category? category2 = _context.Categories.Where(u => u.Id == id).FirstOrDefault();
+            if (category == null)
+            {
+                return NotFound("Category Not Found");
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound("Category Not Found");
+            }
+            Category? category = _context.Categories.Find(id); 
+            if (category == null)
+            {
+                return NotFound("Category Not Found");
+            }
+            return View(category);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteCategory(int? id)
+        {
+            Category? category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound("Category Not Found");
+            }
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
